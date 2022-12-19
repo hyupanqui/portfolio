7b7954d4a1ff56e5e8b55c81f281f93b6306bb0a
Humberto Yupanqui <npmyupanqui@gmail.com>
2022-12-18 20:18:52 -0500

    add svg Moon

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
-const btnSwitch = document.querySelector('#switch');
-btnSwitch.addEventListener('click', () => {
-  document.body.classList.toggle('dark');
-  btnSwitch.classList.toggle('active');
-})
+ const btnSwitch = document.getElementById('switch');
+
+ btnSwitch.addEventListener('click', () => {
+   document.body.classList.toggle('dark');
+   btnSwitch.classList.toggle('active');
+ })
+
+// const btnSwitch = document.getElementById('switch');
+// const body = document.body;
+
+// btnSwitch.addEventListener('click', function() {
+//   const pers1 = body.classList.toggle('dark')
+//   const pers2 = localStorage.setItem('switch', pers1)
+//   btnSwitch.classList.toggle('active', pers2);
+// })
+
+// const valor1 = localStorage.getItem('switch')
+
+// if (valor1 == "true" ) {
+//   body.classList.add('dark')
+// } else {
+//   body.classList.remove('dark')
+// }
